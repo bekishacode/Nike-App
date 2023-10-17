2b872290d5a4f0eefe097598d6ae933810398a93
Bereket Fikre <bekifikre19@gmail.com>
2023-10-18 00:54:04 +0300

    Updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section id="home" className="
+    w-full border-2 border-red-500 p-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container ">
+
+      <div className="relative xl:w-2/5 flex flex-col justify-center items=start w-full max-xl padding-x pt-28">
+        <p>Our Summer Collections</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br/>
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
+      </div>
+
+    </section>
   )
 }
 
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from './Hero';
+import Footer from './Footer';
+import Navbar from './Navbar';
+import CustomerReviews from './CustomerReviews';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import Services from './Services';
+import Subscribe from './Subscribe';
+import SpecialOffer from './SpecialOffer';
+
+export{
+    CustomerReviews,
+    Footer,
+    Hero,
+    Navbar,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe, SuperQuality
+};
