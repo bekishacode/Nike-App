ff6d7d36cad6f592c6505c2325630b28f0862095
Bereket Fikre <bekifikre19@gmail.com>
2023-10-19 15:56:47 +0300

    Popular Products Updated

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,9 +12,9 @@ const Hero = () => {
 
   return (
     <section id="home" className="
-    w-full flex xl:flex-row flex-col justify-center min-h-screen gap-14 max-container ">
+    w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container ">
 
-      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl padding-x pt-28">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
 
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] font-bold">
@@ -41,8 +41,8 @@ const Hero = () => {
         <img
           src={bigShoeImage}
           alt="shoe collection"
-          width={450}
-          height={400}
+          width={610}
+          height={500}
           className="object-contain relative z-10"
         />
         <div className="flex sm:gap-6 gap-4 absolute -bottom-[-1%] sm:left-[10%] max-sm:px-6">
