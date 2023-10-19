58cfc735332349304cbc0068c7a76fea15a5251f
Bereket Fikre <bekifikre19@gmail.com>
2023-10-19 11:20:14 +0300

    Final Upfate on Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,20 +3,24 @@ import {arrowRight} from '../assets/icons';
 import {shoes, statistics} from '../constants';
 import { bigShoe1 } from "../assets/images";
 import ShoeCard from "../components/ShoeCard";
+import { useState } from "react";
 
 
 const Hero = () => {
+  const [bigShoeImage, setbigShoeImage] = useState(bigShoe1)
+
+
   return (
     <section id="home" className="
-    w-full border-2 border-red-500 p-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container ">
+    w-full flex xl:flex-row flex-col justify-center min-h-screen gap-14 max-container ">
 
-      <div className="relative xl:w-2/5 flex flex-col justify-center items=start w-full max-xl padding-x pt-28">
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl padding-x pt-28">
         <p className="text-xl font-montserrat text-coral-red">Our Summer Collections</p>
 
-        <h1 className="mt-10 font-palanquin text-4xl max-sm:text-[72px] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
           <br/>
-          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+          <span className="text-coral-red inline-block mt-2">Nike</span> Shoes
         </h1>
 
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">Discover stylish Nike arrivals, quality comfort, and innovation for your active life.</p>
@@ -35,7 +39,7 @@ const Hero = () => {
 
       <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
+          src={bigShoeImage}
           alt="shoe collection"
           width={450}
           height={400}
@@ -46,8 +50,8 @@ const Hero = () => {
             <div key={shoe}>
               <ShoeCard
                 imgURL = {shoe}
-                changeBigShoeImage = {() =>{}}
-                bigShoeImage = ''
+                changeBigShoeImage = {(shoe) =>setbigShoeImage(shoe)}
+                bigShoeImage = {bigShoeImage}
               />
             </div>
           ))}
